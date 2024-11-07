636c5af5f0d6f9eaba94699b80113d1c10534b79
Shakur <shakur@lastbot.com>
2024-11-07 14:28:46 +0200

    add api login endpoint

---
+++ src/pages/api/login.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    const validPasswords = process.env.HOMEPAGE_PASSWORDS
+      ? process.env.HOMEPAGE_PASSWORDS.split(",")
+      : [];
+
+    if (validPasswords.includes(password)) {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ message: "Invalid password" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
