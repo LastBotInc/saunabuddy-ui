3864064055f84d60c51507283287f90c11f3088d
Shakur <shakur@lastbot.com>
2024-11-12 15:06:14 +0200

    update server url

--- src/hooks/useConnection.tsx
+++ src/hooks/useConnection.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { useCloud } from "@/cloud/useCloud";
 import React, { createContext, useState } from "react";
 import { useCallback } from "react";
 import { useConfig } from "./useConfig";
 
-export type ConnectionMode = "cloud" | "manual" | "env"
+export type ConnectionMode = "cloud" | "manual" | "env";
 
 type TokenGeneratorData = {
   shouldConnect: boolean;
@@ -13,10 +13,15 @@ type TokenGeneratorData = {
   token: string;
   mode: ConnectionMode;
   disconnect: () => Promise<void>;
-  connect: (mode: ConnectionMode, opts?: { language?: string }) => Promise<void>;
+  connect: (
+    mode: ConnectionMode,
+    opts?: { language?: string }
+  ) => Promise<void>;
 };
 
-const ConnectionContext = createContext<TokenGeneratorData | undefined>(undefined);
+const ConnectionContext = createContext<TokenGeneratorData | undefined>(
+  undefined
+);
 
 export const ConnectionProvider = ({
   children,
@@ -36,46 +41,45 @@ export const ConnectionProvider = ({
     setConnectionDetails((prev) => ({ ...prev, shouldConnect: false }));
   }, []);
 
+  const connect = useCallback(
+    async (
+      mode: ConnectionMode,
+      opts?: { language?: string; serverUrl?: string }
+    ) => {
+      console.log("Connect called with mode:", mode, "and opts:", opts);
+      let token = "";
+      let url = opts?.serverUrl || "";
+      if (mode === "cloud") {
+        token = await generateToken();
+        url = cloudWSUrl;
+      } else if (mode === "env") {
+        if (!process.env.NEXT_PUBLIC_LIVEKIT_URL) {
+          throw new Error("NEXT_PUBLIC_LIVEKIT_URL is not set");
+        }
 
-  const connect = useCallback(async (mode: ConnectionMode, opts?: { language?: string }) => {
-    console.log("Connect called with mode:", mode, "and opts:", opts);
-    let token = "";
-    let url = "";
-    if (mode === "cloud") {
-      token = await generateToken();
-      url = cloudWSUrl;
-    } else if (mode === "env") {
-      if(!process.env.NEXT_PUBLIC_LIVEKIT_URL) {
-        throw new Error("NEXT_PUBLIC_LIVEKIT_URL is not set");
+        if (!url) {
+          throw new Error("NEXT_PUBLIC_SERVER_URL is not set");
+        }
+        const uuid = Math.random().toString(36).substring(4);
+        const serverUrl = new URL(url);
+        serverUrl.searchParams.append("uuid", uuid);
+        if (opts?.language) {
+          serverUrl.searchParams.append("language", opts.language);
+        }
+        console.log("Server URL with params:", serverUrl.toString());
+        const response = await fetch(serverUrl.toString());
+        const data = await response.json();
+        token = data.token;
+        url = data.url;
+      } else {
+        token = config.settings.token;
+        url = config.settings.ws_url;
       }
-
-      url = process.env.NEXT_PUBLIC_LIVEKIT_URL;
-      if(!process.env.NEXT_PUBLIC_SERVER_URL) {
-        throw new Error("NEXT_PUBLIC_SERVER_URL is not set");
-      }
-      const uuid = Math.random().toString(36).substring(4);
-      const serverUrl = new URL(process.env.NEXT_PUBLIC_SERVER_URL);
-      serverUrl.searchParams.append('uuid', uuid);
-      if (opts?.language) {
-        serverUrl.searchParams.append('language', opts.language);
-      }
-      console.log("Server URL with params:", serverUrl.toString());
-      const response = await fetch(serverUrl.toString());
-      const data = await response.json();
-      token = data.token;
-      url = data.url;
-    } else {
-      token = config.settings.token;
-      url = config.settings.ws_url;
-    }
-    console.log("Connecting with URL:", url, "and token:", token);
-    setConnectionDetails({ wsUrl: url, token, shouldConnect: true, mode });
-  }, [
-    cloudWSUrl,
-    config.settings.token,
-    config.settings.ws_url,
-    generateToken,
-  ]);
+      console.log("Connecting with URL:", url, "and token:", token);
+      setConnectionDetails({ wsUrl: url, token, shouldConnect: true, mode });
+    },
+    [cloudWSUrl, config.settings.token, config.settings.ws_url, generateToken]
+  );
 
   return (
     <ConnectionContext.Provider
@@ -99,4 +103,4 @@ export const useConnection = () => {
     throw new Error("useConnection must be used within a ConnectionProvider");
   }
   return context;
-}
+};
